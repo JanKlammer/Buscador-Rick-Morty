2a948d17a3a1bac5d2e789eb3e8c0a4a8694a917
Jan <klammerjan18@gmail.com>
2024-11-27 09:41:38 -0300

    1 que funciona (info ok, cards mal centradas y fotos tamaño mal)

--- app/layers/services/services.py
+++ app/layers/services/services.py
@@ -1,24 +1,57 @@
 # capa de servicio/lógica de negocio
 
+
 from ..persistence import repositories
 from ..utilities import translator
 from django.contrib.auth import get_user
+from ..transport import transport
+
 
 def getAllImages(input=None):
     # obtiene un listado de datos "crudos" desde la API, usando a transport.py.
-    json_collection = []
-
-    # recorre cada dato crudo de la colección anterior, lo convierte en una Card y lo agrega a images.
     images = []
+    json_collection = transport.getAllImages()
+    # json_collection = [] # Guardo aca el json en crudo que es el que estamos ahora renderizando y por eso las cards se ven medio mal
+    # Falta el codigo que itera sobre el json y lo convierte en las cards para que use el home
+    # Fijarse en el home que datos son los que se usan de la card
+    for element in json_collection:
+        # Extraemos la información que necesitamos de cada personaje
+       
+        #1. Nombre del personaje.
+        name = element.get('name', 'Nombre no disponible')  
+        
+        #2. Estado del personaje (vivo, muerto o desconocido)
+        status = element.get('status', 'Estado no disponible')  
+       
+        #3. Ultima ubicación del personaje
+        last_location = element.get('location', {}).get('name', 'Ubicación no disponible') 
+        
+        #4. Ubicación de la primera aparición del personaje.
+        first_seen = element.get('origin', {}).get('name', 'unknown')
+       
+        #5. Imagen del personaje.
+        image = element.get("image", "Imagen no disponible")
+       
+        # Creamos una "card" con los datos obtenidos
+        images.append({
+            'name': name,
+            'status': status,
+            'last_location': last_location,
+            "first_seen": first_seen,
+            'url': image #esto era "image" : image y no salia nada, al cambiarlo a "url":image se corrigio.
+            })
 
     return images
+
 
 # añadir favoritos (usado desde el template 'home.html')
 def saveFavourite(request):
     fav = '' # transformamos un request del template en una Card.
     fav.user = '' # le asignamos el usuario correspondiente.
 
+
     return repositories.saveFavourite(fav) # lo guardamos en la base.
+
 
 # usados desde el template 'favourites.html'
 def getAllFavourites(request):
@@ -27,15 +60,20 @@ def getAllFavourites(request):
     else:
         user = get_user(request)
 
+
         favourite_list = [] # buscamos desde el repositories.py TODOS los favoritos del usuario (variable 'user').
         mapped_favourites = []
+
 
         for favourite in favourite_list:
             card = '' # transformamos cada favorito en una Card, y lo almacenamos en card.
             mapped_favourites.append(card)
 
+
         return mapped_favourites
+
 
 def deleteFavourite(request):
     favId = request.POST.get('id')
     return repositories.deleteFavourite(favId) # borramos un favorito por su ID.
+
--- app/layers/transport/transport.py
+++ app/layers/transport/transport.py
@@ -1,22 +1,29 @@
 # capa de transporte/comunicación con otras interfaces o sistemas externos.
-
-import requests
 from ...config import config
-
 # comunicación con la REST API.
 # este método se encarga de "pegarle" a la API y traer una lista de objetos JSON crudos (raw).
+
+#from .transport import getAllImages as fetch_data_from_api
+import requests
+
+
 def getAllImages(input=None):
+    
     if input is None:
         json_response = requests.get(config.DEFAULT_REST_API_URL).json()
     else:
         json_response = requests.get(config.DEFAULT_REST_API_SEARCH + input).json()
+    
 
+    #json_collection = fetch_data_from_api(input)
     json_collection = []
+
 
     # si la búsqueda no arroja resultados, entonces retornamos una lista vacía de elementos.
     if 'error' in json_response:
         print("[transport.py]: la búsqueda no arrojó resultados.")
         return json_collection
+
 
     for object in json_response['results']:
         try:
@@ -25,8 +32,11 @@ def getAllImages(input=None):
             else:
                 print("[transport.py]: se encontró un objeto sin clave 'image', omitiendo...")
 
-        except KeyError: 
+
+        except KeyError:
             # puede ocurrir que no todos los objetos tenga la info. completa, en ese caso descartamos dicho objeto y seguimos con el siguiente en la próxima iteración.
             pass
 
+
     return json_collection
+
